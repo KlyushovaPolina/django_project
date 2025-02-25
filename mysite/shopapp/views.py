1f5c2ba46588900079dde17fb08ad51196478ce4
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-02-26 01:25:21 +0300

    3.4 добавлено поле user в таблицу order  (связь многие к одному)

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 
 from timeit import default_timer
 
@@ -22,3 +22,9 @@ def products_list(request: HttpRequest):
         "products": Product.objects.all()
     }
     return render(request, 'shopapp/products-list.html', context = context)
+
+def orders_list(request: HttpRequest):
+    context = {
+        'orders': Order.objects.select_related("user").all()
+    }
+    return render(request, 'shopapp/orders-list.html', context = context)
