2dec9c051a896aa2302e5e7f9c7ebdb6ca46f9d7
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-02-26 00:35:19 +0300

    3.4 создана таблица order

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,9 @@ class Product(models.Model):
     discount = models.SmallIntegerField (default = 0)
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
+
+class Order(models.Model):
+    delivery_address = models.TextField(null=False, blank = True)
+    promocode = models.CharField(max_length=20, null = False, blank = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
