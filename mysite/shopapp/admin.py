d2909e4140884b8686734c557fc5ce297bcc6b2c
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-03-03 23:43:54 +0300

    4.4 finished

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -2,15 +2,30 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product
+from .models import Product, Order
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
     #list_display = "pk", "name", "description", "price", "discount"
     list_display = "pk", "name", "description_short", "price", "discount"
     list_display_links = "pk", "name"
     ordering = "pk",
     search_fields = "name", "description"
+    fieldsets = [
+        (None, {
+            "fields":("name", "description"),
+        }),
+        ("price_options",{
+            "fields":("price", "discount"),
+            "classes":("collapse","wide")
+        })
+    ]
 
     def description_short(self, obj:Product) -> str:
         if len(obj.description) < 48:
@@ -18,3 +33,20 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.description[:48]+"..."
 
 #admin.site.register(Product, ProductAdmin)
+
+# class ProductInline(admin.TabularInline):
+class ProductInline(admin.StackedInline):
+    model = Order.products.through
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline
+    ]
+    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj:Order) -> str:
+        return obj.user.first_name or obj.user.username
