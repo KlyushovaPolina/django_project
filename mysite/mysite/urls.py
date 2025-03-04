e392e8d2d13e741481d5a7e2c34f2f5f4499f331
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-03-04 12:34:22 +0300

    5.3 finished

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
+    path('req/', include('requestdataapp.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
