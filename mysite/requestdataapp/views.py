2d1e87c601537277e325a4c80a7492baad86950e
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-03-11 13:46:26 +0300

    6.3 finished

--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -1,7 +1,7 @@
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from .forms import UserBioForm
+from .forms import UserBioForm, UploadFileForm
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
     a = request.GET.get('a', '')
@@ -21,9 +21,23 @@ def user_form(request: HttpRequest) -> HttpResponse:
     return render(request, 'requestdataapp/user-bio-form.html', context)
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    if request.method=="POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        print ("saved file", filename)
-    return render(request, 'requestdataapp/file-upload.html')
+    # form = UploadFileForm()
+    # if request.method=="POST" and request.FILES.get("myfile"):
+    #     myfile = request.FILES["myfile"]
+    #     fs = FileSystemStorage()
+    #     filename = fs.save(myfile.name, myfile)
+    #     print ("saved file", filename)
+
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            myfile = form.cleaned_data["file"]
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            print("saved file", filename)
+    else:
+        form = UploadFileForm()
+    context={
+        "form":form,
+    }
+    return render(request, 'requestdataapp/file-upload.html', context = context)
