0d00c1111a3cad9c25d64116142fdb3126fce857
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2025-03-20 15:49:40 +0300

    8.5 finished

--- mysite/shopapp/urls.py
+++ mysite/shopapp/urls.py
@@ -8,7 +8,8 @@ from .views import (
     ProductsListView,
     OrdersListView,
     OrderDetailView,
-    create_product,
+    ProductCreateView,
+    ProductUpdateView,
     create_order)
 
 app_name = "shopapp"
@@ -17,9 +18,10 @@ urlpatterns = [
     path("", ShopIndexView.as_view(), name = 'index'),
     path("groups/", GroupsListView.as_view(), name = 'groups_list'),
     path("products/", ProductsListView.as_view(), name = 'products_list'),
+    path("products/create", ProductCreateView.as_view(), name='product_create'),
     path("products/<int:pk>/", ProductDetailsView.as_view(), name = 'product_details'),
+    path("products/<int:pk>/update", ProductUpdateView.as_view(), name='product_update'),
     path("orders/", OrdersListView.as_view(), name = 'orders_list'),
+    path("orders/create", create_order, name='order_create'),
     path("orders/<int:pk>/", OrderDetailView.as_view(), name='order_details'),
-    path("products/create", create_product, name='product_create'),
-    path("orders/create", create_order, name = 'order_create'),
 ]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 
 from .models import Product, Order
 from .forms import ProductForm, OrderForm, GroupForm
@@ -75,20 +76,36 @@ class ProductsListView(ListView):
 #     }
 #     return render(request, 'shopapp/products-list.html', context = context)
 
-def create_product(request: HttpRequest) -> HttpResponse:
-    if request.method =='POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            #Product.objects.create(**form.cleaned_data)
-            form.save()
-            url = reverse("shopapp:products_list")
-            return redirect(url)
-    else:
-        form = ProductForm()
-    context={
-        "form":form,
-    }
-    return render(request, 'shopapp/create-product.html', context=context)
+class ProductCreateView(CreateView):
+    model = Product
+    fields = "name", "price", "description", "discount"
+    success_url = reverse_lazy("shopapp:products_list")
+
+# def create_product(request: HttpRequest) -> HttpResponse:
+#     if request.method =='POST':
+#         form = ProductForm(request.POST)
+#         if form.is_valid():
+#             #Product.objects.create(**form.cleaned_data)
+#             form.save()
+#             url = reverse("shopapp:products_list")
+#             return redirect(url)
+#     else:
+#         form = ProductForm()
+#     context={
+#         "form":form,
+#     }
+#     return render(request, 'shopapp/create-product.html', context=context)
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = "name", "price", "description", "discount"
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return(reverse(
+            "shopapp:product_details",
+            kwargs={"pk":self.object.pk}
+        ))
 
 class OrdersListView(ListView):
     queryset = (
